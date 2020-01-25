189a6c8637252291d234eacf75acb565f2be03fb
Rodrigo <rodrigomaximocano@gmail.com>
2020-01-24 22:20:52 -0300

    Add routes to plans

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import PlanController from './app/controllers/PlanController';
 import StudentController from './app/controllers/StudentController';
 import authMiddleware from './app/middlewares/auth';
 
@@ -12,6 +13,10 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.post('/students', StudentController.store);
+routes.post('/plan', PlanController.store);
+routes.get('/plan', PlanController.list);
+routes.put('/plan', PlanController.update);
+routes.delete('/plan', PlanController.delete);
 routes.put('/users', UserController.update);
 
 export default routes;
