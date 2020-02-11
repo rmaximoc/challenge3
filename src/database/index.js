9ec062f3dcd9fe7cfe939c295deb9807af3e1f91
Rodrigo <rodrigomaximocano@gmail.com>
2020-02-11 13:11:23 -0300

    Add multer configuration

--- src/database/index.js
+++ src/database/index.js
@@ -3,8 +3,9 @@ import databaseConfig from '../config/database';
 import User from '../app/models/User';
 import Recipient from '../app/models/Recipient';
 import Courier from '../app/models/Courier';
+import File from '../app/models/File';
 
-const models = [User, Recipient, Courier];
+const models = [User, Recipient, Courier, File];
 
 class Database {
   constructor() {
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import CourierController from './app/controllers/CourierController';
 import RecipientController from './app/controllers/RecipientController';
+import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -18,5 +22,7 @@ routes.get('/couriers', CourierController.list);
 routes.put('/couriers', CourierController.update);
 routes.delete('/couriers', CourierController.delete);
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
